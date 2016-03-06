ced194716d59470e6a10f48f56177d28fa78b9bf
Norbert de Langen <norbert@macaw.nl>
2016-03-06 13:51:44 +0100

    CHANGE wrap.js so error output is 'escaped' & ADD log for the error

--- core/middlewares/wrap.js
+++ core/middlewares/wrap.js
@@ -18,115 +18,116 @@ var specUtils = require(path.join(global.pathToApp,'core/lib/specUtils'));
 * */
 exports.process = function (req, res, next) {
 
-	// Check if we're working with processed file
-	if (req.specData && req.specData.renderedHtml) {
-		var specDir = specUtils.getFullPathToSpec(req.path);
-		var contextOptions = req.specData.contextOptions;
+    // Check if we're working with processed file
+    if (req.specData && req.specData.renderedHtml) {
+        var specDir = specUtils.getFullPathToSpec(req.path);
+        var contextOptions = req.specData.contextOptions;
 
-		// get spec content
-		var data = req.specData.renderedHtml.replace(/^\s+|\s+$/g, '');
+        // get spec content
+        var data = req.specData.renderedHtml.replace(/^\s+|\s+$/g, '');
 
-		// get spec info
-		var info = req.specData.info;
+        // get spec info
+        var info = req.specData.info;
 
-		// choose the proper template, depending on page type or defined path
-		var viewParam = 'spec';
-		var context;
+        // choose the proper template, depending on page type or defined path
+        var viewParam = 'spec';
+        var context;
 
-		if (info.template) {
-			viewParam = info.template;
-			context = specDir;
-		} else if (info.role === 'navigation') {
-			viewParam = 'navigation';
-		}
+        if (info.template) {
+            viewParam = info.template;
+            context = specDir;
+        } else if (info.role === 'navigation') {
+            viewParam = 'navigation';
+        }
 
-		// Check if we're working with included dirs
-		if (context) {
-			global.opts.core.common.includedDirs.forEach(function (includedDir) {
-				var pathToCheck = (path.join(global.userPath, includedDir) + '/').replace(/\\/g, '/');
+        // Check if we're working with included dirs
+        if (context) {
+            global.opts.core.common.includedDirs.forEach(function (includedDir) {
+                var pathToCheck = (path.join(global.userPath, includedDir) + '/').replace(/\\/g, '/');
 
-				if (context.indexOf(pathToCheck) === 0) {
-					context = context.replace(pathToCheck, path.join(global.pathToApp, includedDir) + '/');
-				}
-			});
-		}
+                if (context.indexOf(pathToCheck) === 0) {
+                    context = context.replace(pathToCheck, path.join(global.pathToApp, includedDir) + '/');
+                }
+            });
+        }
 
-		var templatePath = viewResolver(viewParam, contextOptions.rendering.views, context) || viewParam;
+        var templatePath = viewResolver(viewParam, contextOptions.rendering.views, context) || viewParam;
 
-		fs.readFile(templatePath, "utf-8", function(err, template){
-			if (err) {
-				res.send('EJS template "' + templatePath + '" not found. Please check view configuration.');
+        fs.readFile(templatePath, "utf-8", function(err, template){
+            if (err) {
+                res.send('EJS template "' + templatePath + '" not found. Please check view configuration.');
 
-				return;
-			}
+                return;
+            }
 
-			var $ = cheerio.load(data, {decodeEntities: false});
+            var $ = cheerio.load(data, {decodeEntities: false});
 
-			var head = '';
-			var $headHook = $('head');
+            var head = '';
+            var $headHook = $('head');
 
-			if ($headHook.length > 0) {
-				head = $headHook.html();
+            if ($headHook.length > 0) {
+                head = $headHook.html();
 
-				$('head').remove();
-			}
+                $('head').remove();
+            }
 
-			var content = '';
-			var $body = $('body');
-			var $html = $('html');
+            var content = '';
+            var $body = $('body');
+            var $html = $('html');
 
-			if ($body.length > 0) {
-				content = $body.html();
-			} else if ($html.length > 0) {
-				content = $html.html();
-			} else {
-				content = $.html();
-			}
+            if ($body.length > 0) {
+                content = $body.html();
+            } else if ($html.length > 0) {
+                content = $html.html();
+            } else {
+                content = $.html();
+            }
 
-			var heagerFooter = getHeaderAndFooter();
+            var heagerFooter = getHeaderAndFooter();
 
-			// final data object for the template
-			var templateJSON = {
-				engineVersion: global.engineVersion,
-				content: content,
-				head: head,
+            // final data object for the template
+            var templateJSON = {
+                engineVersion: global.engineVersion,
+                content: content,
+                head: head,
                 info: info
-			};
+            };
 
-			try {
-				templateJSON.header = ejs.render(heagerFooter.header, templateJSON);
-			} catch(err){
-				var headerMsg = 'Error: EJS could render header template: ' + heagerFooter.headerPath;
-				templateJSON.header = headerMsg;
-				global.log.warn(headerMsg, err.stack);
-			}
+            try {
+                templateJSON.header = ejs.render(heagerFooter.header, templateJSON);
+            } catch(err){
+                var headerMsg = 'Error: EJS could render header template: ' + heagerFooter.headerPath;
+                templateJSON.header = headerMsg;
+                global.log.warn(headerMsg, err.stack);
+            }
 
-			try {
-				templateJSON.footer = ejs.render(heagerFooter.footer, templateJSON, {
-					filename: heagerFooter.footerPath
-				});
-			} catch(err){
-				var footerMsg = 'Error: EJS could render footer template: ' + heagerFooter.footerPath;
-				templateJSON.footer = footerMsg;
-				global.log.warn(footerMsg, err.stack);
-			}
+            try {
+                templateJSON.footer = ejs.render(heagerFooter.footer, templateJSON, {
+                    filename: heagerFooter.footerPath
+                });
+            } catch(err){
+                var footerMsg = 'Error: EJS could render footer template: ' + heagerFooter.footerPath;
+                templateJSON.footer = footerMsg;
+                global.log.warn(footerMsg, err.stack);
+            }
 
-			// render page and send it as response
-			try {
-				req.specData.renderedHtml = ejs.render(template, templateJSON, {
-					filename: templatePath
-				});
-			} catch(err){
-				req.specData.renderedHtml = 'Error rendering Spec with EJS: ' + template;
-			}
+            // render page and send it as response
+            try {
+                req.specData.renderedHtml = ejs.render(template, templateJSON, {
+                    filename: templatePath
+                });
+            } catch(err){
+                console.log(err);
+                req.specData.renderedHtml = 'Error rendering Spec with EJS: <noscript>' + template + '</noscript>';
+            }
 
 
-			req.specData.renderedHtml += '\n<!-- SourceJS version: ' + global.engineVersion + ' -->';
+            req.specData.renderedHtml += '\n<!-- SourceJS version: ' + global.engineVersion + ' -->';
 
-			next();
-		});
-	} else {
-		// proceed to next middleware
-		next();
-	}
+            next();
+        });
+    } else {
+        // proceed to next middleware
+        next();
+    }
 };
